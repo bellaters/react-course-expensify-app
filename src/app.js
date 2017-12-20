edbb77b31dd58d8343e4604e1ce7a6739011fd9d
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-20 10:39:58 +0100

    clean up references

--- src/app.js
+++ src/app.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>Hey! I am up and running</p>, document.getElementById('app'));
 
