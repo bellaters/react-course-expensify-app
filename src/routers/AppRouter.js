9b579b737120dcb86265b9cb240d16f890bd6ea6
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-20 22:05:31 +0100

    small clean up

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,11 +8,6 @@ import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
-// CHALLENGE 80.1
-// Create 6 new files for 6 new components
-// setup imports, components, default export
-// import into AppRouter so they can beused
-
 const AppRouter = () => (
     <BrowserRouter>
         <div>
