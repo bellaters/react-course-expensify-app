f8744b59a5c803cbfbdf357f58b7af7298c293ef
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-21 23:36:22 +0100

    First Action Store example

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,8 +3,32 @@ import {createStore} from 'redux';
 const log = console.log;
 log('running');
 
-const store = createStore((state = {count:0})=>{
+const store = createStore((state = {
+    count: 0
+}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: state.count = 0
+            };
+        default:
+            return state;
+    }
+
     return state;
 });
 
-log('store.getState()',store.getState());
+log(store.getState());
+
+// Action - an object that gets sent to the store. 
+// For example increment, decrement, reset.
+store.dispatch({type: 'INCREMENT'});
+log(store.getState());
