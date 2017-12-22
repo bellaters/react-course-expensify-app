580ed98df665f17eca35084100cd16130b24bdf4
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-22 01:38:02 +0100

    array destructering

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,7 +1,9 @@
 const log = console.log;
-log('running');
+/////////////////////////////////////////////
+// Object destructuring
+/////////////////////////////////////////////
 
-const person = {
+/* const person = {
     //name: 'Isabel',
     age: 37,
     location: {
@@ -18,4 +20,42 @@ log(`${name} is ${age} years old`);
 //renaming keys
 const { temp: temperatur, city } = person.location;
 log(`In ${city} it is ${temperatur} degree.`);
+ */
+ const book = {
+    title: 'Ego is the enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+ };
+const { name : pName = 'Self-Publisher' } = book.publisher 
+// CHALLENGE 88.1
+log(pName); // Penguin - default Self-Publisher
+
+/////////////////////////////////////////////
+// Array destructuring
+////////////////////////////////////////////
+const address = ['Wilseder Weg 54', 'Düsseldorf', 'NRW', '40468'];
+log(address);
+const [street, city, state, plz] = address;
+console.log(`You are in ${address[1]} ${address[2]}.`);
+console.log(`You are in ${city} ${state}.`);
+
+const [, ,stateOnly = 'Unknown'] = address;
+log(stateOnly);
+
+// CHALLENGE 88.2
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+//grab first and third item using array destructering 
+const [name = 'Drink', , price] = item;
+log('A medium Coffee (hot) costs $2.50');
+log(`A medium ${name} costs ${price}`);
+
+
+
+
+
+
+
+
 
