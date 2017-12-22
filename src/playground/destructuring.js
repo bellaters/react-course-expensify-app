eac0625e0940781b23ba02a85e9fad5a57f6bc87
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-22 01:05:47 +0100

    first practices

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,21 @@
 const log = console.log;
-
 log('running');
+
+const person = {
+    //name: 'Isabel',
+    age: 37,
+    location: {
+        city: 'Düsseldorf',
+        temp: 2
+    }
+};
+
+log(`${person.name} is ${person.age} years old`);
+// destructuring person means for example:
+// set direct default values
+const { name = 'Anonymus', age } = person;
+log(`${name} is ${age} years old`);
+//renaming keys
+const { temp: temperatur, city } = person.location;
+log(`In ${city} it is ${temperatur} degree.`);
+
