55382d687dd476a9a57b71a7c0dadd36814603a6
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-22 14:14:57 +0100

    create addExpense reducer and call it

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,26 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid'
 
 const log = console.log;
-
 // We will need combineReducers as we have to deal 
 // with a lot of different states and actions:
 // ADD_EXPENSE
+const addExpense = (
+    {
+       desciption = '', 
+       note = '', 
+       amount = 0, 
+       createAt = 0
+    } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        desciption,
+        note,
+        amount,
+        createAt
+    }
+});
 // REMOVE_EXPENSE
 // EDIT_EXPENSIVE
 // SET_TEXT_FILTER
@@ -17,6 +33,8 @@ const log = console.log;
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch(action.type) {
+        case 'ADD_EXPENSE':
+            return state.concat(action.expense);
         default:
             return state;
     }
@@ -43,22 +61,9 @@ const store = createStore(
         filters: filtersReducer
     })
 );
-log('Initial State of Store: ',store.getState());
+store.subscribe(()=>{
+    log('On Store Dispatch: ',store.getState());
+});
 
+store.dispatch(addExpense({desciption: 'Rent', amount: '100'}));
 
-const demoState = {
-    expenses: [{
-        id: 'foo',
-        desciption: 'January Rent',
-        note: 'This was the final payment for the address',
-        amount: 54500, //given in cent ;)
-        createAt: 0
-    }],
-    filters: {
-        text: 'rent',
-        sortBy: 'amount', //date or amount
-        startDate: undefined,
-        endDate: undefined
-    }
-};
-log('demoState: ', demoState);
