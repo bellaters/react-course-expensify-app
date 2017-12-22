99299a3671c203915dfe6141a53e83c8814b7ff9
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-22 12:45:54 +0100

    create demoState

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,1 +1,20 @@
-console.log('running');
+import {createStore, combineReducers} from 'redux';
+
+const log = console.log;
+log('running');
+const demoState = {
+    expenses: [{
+        id: 'foo',
+        desciption: 'January Rent',
+        note: 'This was the final payment for the address',
+        amount: 54500, //given in cent ;)
+        createAt: 0
+    }],
+    filters: {
+        text: 'rent',
+        sortBy: 'amount', //date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
+};
+console.log(demoState);
