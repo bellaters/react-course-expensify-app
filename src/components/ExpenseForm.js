dba8cb8a8d15be0208fba40be7dabb9268ed99ba
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-29 22:11:38 +0100

    // CHALLENGE 105     // setup note state      // setup onChange and value for textarea

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
 export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: ''
+    };
+    // CHALLENGE 105
+    // setup note state 
+    // setup onChange and value for textarea
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState( () => ({ note }));
+    };
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState( () => ({ description }));
+    };
     render() {
         return (
             <div>
-                ExpenseForm
+                <form>
+                    <input 
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input 
+                        type="number"
+                        placeholder="Amount"
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
             </div>
         )
     }
