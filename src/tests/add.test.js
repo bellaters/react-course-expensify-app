b559a8faae402897d1b58ce1180f464c3fceb739
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-30 03:17:14 +0100

    first expect toBe usage

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -2,8 +2,5 @@ const add = (a, b) => a+b;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
-    if(result !== 7){
-        throw new Error('3 + 4 schould be 7!');
-        
-    }
+    expect(result).toBe(7);
 });
