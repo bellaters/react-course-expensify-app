ea2979ecb6ae699eced434d161d2b5989c01a70f
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-30 05:06:38 +0100

    test sortByDate and sortByAmount

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import {setStartDate, setEndDate, setTextFilter} from '../../actions/filters';
+import {setStartDate, setEndDate, setTextFilter, sortByAmount, sortByDate} from '../../actions/filters';
 
 test('should generate set start date action object', () => {
     const action = setStartDate(moment(0));
@@ -32,5 +32,17 @@ test('should generate set text filter action with default value', () => {
     });
 });
 
+test('should generate sort by date filter action', () => {
+    const action = sortByDate();
+    expect(action).toEqual({
+        type: 'SORT_BY_DATE',
+    });
+});
+test('should generate sort by amount filter action', () => {
+    const action = sortByAmount();
+    expect(action).toEqual({
+        type: 'SORT_BY_AMOUNT',
+    });
+});
 
 
