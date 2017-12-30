fb63225f845d5890970be8d6cead00ba2a02e4ee
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-30 03:15:04 +0100

    first error at a test

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -2,5 +2,8 @@ const add = (a, b) => a+b;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
-    
+    if(result !== 7){
+        throw new Error('3 + 4 schould be 7!');
+        
+    }
 });
