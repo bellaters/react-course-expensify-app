4d43685e8885b720f02552801b0102e884fbfb56
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-30 05:08:10 +0100

    clean up

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,11 @@
 import moment from 'moment';
-import {setStartDate, setEndDate, setTextFilter, sortByAmount, sortByDate} from '../../actions/filters';
+import {
+    setStartDate, 
+    setEndDate, 
+    setTextFilter, 
+    sortByAmount, 
+    sortByDate} 
+    from '../../actions/filters';
 
 test('should generate set start date action object', () => {
     const action = setStartDate(moment(0));
