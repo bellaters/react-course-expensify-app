4cba2282cdb686ddd3b1a1d8de56b57a3f1c3aaa
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-31 03:21:46 +0100

    setup enzyme with react 16

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,5 +5,5 @@ import Header from '../../components/Header';
 test('should render Header correctly', () => {
     const renderer = new ReactShallowRenderer();
     renderer.render(<Header/>);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    expect(renderer.getRenderOutput()).toMatchSnapshot();   
 });
