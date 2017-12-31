b2181e8410edbcd69c154cce666ac4be75063159
Isabel Wolters <Isabel.Wolters@net-m.de>
2017-12-31 04:43:36 +0100

    test first components with enzyme

---
+++ src/tests/components/ExpenseListItem.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpenseListItem } from '../../components/ExpenseListItem';
+import expenses from '../fixtures/expenses';
+
+test(' should render ExpenseList with expenses', () => {
+    const expense = expenses[0];
+    const wrapper = shallow(<ExpenseListItem 
+                                key={expense.id}
+                                {...expense}/>
+                    );
+    expect(wrapper).toMatchSnapshot();
+});
