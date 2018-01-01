5d5d8c5c4313f47e13af093ed4855e62d211cd12
bellaters <gibbetnet@hotmail.com>
2018-01-01 20:06:59 +0100

    catch a reject

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,12 +1,18 @@
 const promise = new Promise( (resolve, reject) => {
     setTimeout( () => {
-        resolve('This is my resolved data');
-    }, 2500);
+        /* resolve({
+            name: 'Foo Bar',
+            age: 40
+        }); */
+        reject('Something went wrong!');
+    }, 2000);
     
 });
 
 console.log('before');
 promise.then((data) => {
     console.log('1', data);
+}).catch( (error) => {
+    console.log('error: ', error);
 });
 console.log('after');
