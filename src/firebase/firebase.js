624213ff7698bba865d3db4dee75260215343d56
bellaters <gibbetnet@hotmail.com>
2018-01-01 20:59:58 +0100

    use firebase update to set and remove db entries

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,18 +12,10 @@ messagingSenderId: "1073071908734"
 firebase.initializeApp(config);
 // Getting firebase
 const database = firebase.database();
-database.ref('isSingle')
-    .remove()
-    .then( () => {
-        console.log('isSingle was removed.');
-    }).catch( (e) => {
-        console.log('gnaaaaa... : ', e);
-    });
 
-/* 
 database.ref().set({
     name: 'Foo Bar', 
-    age: 40,
+    age: 42,
     isSingle: false,
     location: {
         city: 'Philadelphia',
@@ -34,4 +26,20 @@ database.ref().set({
 }).catch( (e) => {
     console.log('This failed.', e);
 });
- */
+
+database.ref().update({
+    name: 'Mystery',
+    age: 24,
+    job: 'Software Developer',
+    isSingle: null
+});
+
+/* 
+database.ref('isSingle')
+    .remove()
+    .then( () => {
+        console.log('isSingle was removed.');
+    }).catch( (e) => {
+        console.log('gnaaaaa... : ', e);
+    });
+ */ 
