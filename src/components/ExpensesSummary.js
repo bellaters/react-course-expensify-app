a2d4e1ea4649d9aadac690e7fbbfc13cab452f96
bellaters <gibbetnet@hotmail.com>
2018-01-01 16:51:52 +0100

    ExpensesSummary Implementation

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,25 @@
-// 1. Create ExpencesSummary Component
+// 1. Create ExpensesSummary Component
 // -----------------------------------
+import React from 'react';
+import { connect } from 'react-redux';
+import selectExpensesTotal from '../selectors/expenses-total';
+
+export const ExpensesSummary = (props) => (
+    <div>
+        <p>
+        {`Viewing ${props.expenses.length} expenses 
+        totalling ${selectExpensesTotal(props.expenses)}`}
+        </p>
+    </div>
+);
+
+const mapStateToProps = (state) => {
+    return {
+        expenses: selectExpensesTotal(state.expenses, state.filters)
+    };
+}; 
+
+export default connect(mapStateToProps)(ExpensesSummary);
 
 // Rendered by ExpenseDashboardPage
 // Test with two snapshot tests
