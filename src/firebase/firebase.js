467c2e674eea56b13fbc9b1375330d26bb268b6b
bellaters <gibbetnet@hotmail.com>
2018-01-01 22:18:03 +0100

    first usage of child_added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -19,7 +19,12 @@ database.ref('expenses').on('child_removed', (snapshot) => {
 });
 
 // child_changed
-database.ref('expenses').on('child_removed', (snapshot) => {
+database.ref('expenses').on('child_changed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
     console.log(snapshot.key, snapshot.val());
 });
 
