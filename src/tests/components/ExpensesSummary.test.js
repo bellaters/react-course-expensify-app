2c7b8521b8ab2def5a90e740b6a0acc0b727a4c8
bellaters <gibbetnet@hotmail.com>
2018-01-01 18:37:58 +0100

    wrote snapshot tests for ExpenseSummary

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
 
-test(' ', () => {
 
+test(' should correctly rendere ExpensesSummary with 1 expense', () => {
+    const wrapper = shallow(<ExpensesSummary count={1} total={100} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test(' should correctly rendere ExpensesSummary with multiple expenses', () => {
+    const wrapper = shallow(<ExpensesSummary count={10} total={1000} />);
+    expect(wrapper).toMatchSnapshot();
 });
