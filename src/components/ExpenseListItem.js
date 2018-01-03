2014f8bdff3d4489915282fdc8b007e11da74e98
bellaters <gibbetnet@hotmail.com>
2018-01-03 21:39:01 +0100

    fixed date view in ListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
-export const ExpenseListItem = ({ id, description, amount, createAt}) => (
+export const ExpenseListItem = ({ id, description, amount, createdAt}) => (
     <Link className="list-item" to={`/edit/${id}`}>
         <div>
             <h3>{description}</h3>
-            <span>{moment(createAt).format('MMMM DD, YYYY')}</span>
+            <span>{moment(createdAt).format('MMMM DD, YYYY')}</span>
         </div>
             <h3>{numeral(amount / 100).format('$0,0.00')}</h3>
     </Link>
