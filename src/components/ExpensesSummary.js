024b0c868d402261501b4898d81b0d9f301a85d6
bellaters <gibbetnet@hotmail.com>
2018-01-04 01:37:20 +0100

    cleanup comments

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,3 @@
-// 1. Create ExpensesSummary Component
-// -----------------------------------
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -7,7 +5,7 @@ import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = ({count, total, hidden }) => {
+export const ExpensesSummary = ({count, total, hidden}) => {
     const expenseWord = count === 1 ? 'expense' : 'expenses';
     const formattedTotal = numeral(total / 100).format('$0,0.00');
     return (
@@ -38,20 +36,3 @@ const mapStateToProps = (state) => {
 }; 
 
 export default connect(mapStateToProps)(ExpensesSummary);
-
-// Rendered by ExpenseDashboardPage
-// Test with two snapshot tests
-// Connected to store for:
-//      - expenseCount (how many visible expenses?)
-//      - expensesTotal (what's the total of the visible expenses?)
-
-// Example
-// Viewing 2 expenses totalling $94.34
-
-// Example
-// Viewing 3 expenses totalling $94.34
-
-// 2. Commit and Deploy
-// ------------------------------------
-
-// Get the feature live!
